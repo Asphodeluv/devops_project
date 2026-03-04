c2c6e7251b8a9542eed1f579f602411f0ee5459e
Asphodeluv <seilahaa@gmail.com>
2026-03-04 13:28:58 +0200

    feat(Main.java): Read 2 numbers from input

--- java-scanner/Main.java
+++ java-scanner/Main.java
@@ -3,5 +3,11 @@ import java.util.Scanner;
 public class Main {
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.print("Give number 1: ");
+        double num1 = Double.parseDouble(sc.nextLine());
+
+        System.out.print("Give number 2: ");
+        double num2 = Double.parseDouble(sc.nextLine());
     }
 }
