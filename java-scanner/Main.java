7586c7ea0d78e07d859f572fe52e79e6089fa066
Asphodeluv <seilahaa@gmail.com>
2026-03-04 13:29:54 +0200

    feat(Main.java): Read operator from input

--- java-scanner/Main.java
+++ java-scanner/Main.java
@@ -7,6 +7,9 @@ public class Main {
         System.out.print("Give number 1: ");
         double num1 = Double.parseDouble(sc.nextLine());
 
+        System.out.print("Give operator (+, -, *, /): ");
+        String operator = sc.nextLine();
+
         System.out.print("Give number 2: ");
         double num2 = Double.parseDouble(sc.nextLine());
     }
